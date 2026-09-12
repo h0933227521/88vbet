3f57e6bd32ee151a0193fbdfc4926461b99c9734
h0933227521 <h0933227521@gmail.com>
2026-09-13 02:50:08 +0700

    Update request handling for new origin host

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -1,24 +1,21 @@
 export async function onRequest(context) {
     const request = context.request;
-    const visitorUrl = new URL(request.url);
+    const publicUrl = new URL(request.url);
 
-    // DOMAIN WORDPRESS TRÊN VPS
-    const originHost = "188v.io";
+    const originHost = "riobet-ingk.buzz";
+    const originBase = `https://${originHost}`;
 
-    // Giữ nguyên đường dẫn + query
     const originUrl = new URL(
-        visitorUrl.pathname + visitorUrl.search,
-        `https://${originHost}`
+        publicUrl.pathname + publicUrl.search,
+        originBase
     );
 
-    // Copy headers từ request
     const headers = new Headers(request.headers);
 
-    // Cho origin biết domain public mà khách đang truy cập
-    headers.set("X-Forwarded-Host", visitorUrl.host);
+    headers.set("X-Forwarded-Host", publicUrl.host);
     headers.set("X-Forwarded-Proto", "https");
 
-    const originRequest = new Request(originUrl.toString(), {
+    const response = await fetch(originUrl.toString(), {
         method: request.method,
         headers,
         body:
@@ -28,21 +25,19 @@ export async function onRequest(context) {
         redirect: "manual"
     });
 
-    const response = await fetch(originRequest);
-
     const responseHeaders = new Headers(response.headers);
 
-    // Nếu WordPress redirect về domain gốc,
-    // đổi Location trở lại pages.dev
+    // Chặn redirect về domain VPS
     const location = responseHeaders.get("location");
 
     if (location) {
+        const publicBase = `${publicUrl.protocol}//${publicUrl.host}`;
+
         responseHeaders.set(
             "location",
-            location.replace(
-                `https://${originHost}`,
-                `${visitorUrl.protocol}//${visitorUrl.host}`
-            )
+            location
+                .replace(`https://${originHost}`, publicBase)
+                .replace(`http://${originHost}`, publicBase)
         );
     }
 
